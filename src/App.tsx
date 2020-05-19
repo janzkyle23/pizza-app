51670370fa24fc8ad9cf38777b65ad521ede348a
Kyle Ordona <janzrel.ordona@obf.ateneo.edu>
2020-05-20 04:21:30 +0800

    added the necessary pizza data

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
-import { Container, Box, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Grid, Typography } from '@material-ui/core';
+import Steps from './components/Steps';
+
+type Size = 'Small' | 'Medium' | 'Large';
+type Crust = 'Thin' | 'Thick';
+type Ingredient =
+  | 'Pepperoni'
+  | 'Mushrooms'
+  | 'Onions'
+  | 'Sausage'
+  | 'Bacon'
+  | 'Extra cheese'
+  | 'Black olives'
+  | 'Green peppers'
+  | 'Pineapple'
+  | 'Spinach';
+type Ingredients = {
+  [name in Ingredient]: boolean;
+};
+
+const ingredients: Ingredients = {
+  Pepperoni: false,
+  Mushrooms: false,
+  Onions: false,
+  Sausage: false,
+  Bacon: false,
+  'Extra cheese': false,
+  'Black olives': false,
+  'Green peppers': false,
+  Pineapple: false,
+  Spinach: false,
+};
 
 function App() {
+  const [size, setSize] = useState<Size | null>(null);
+  const [crust, setCrust] = useState<Crust | null>(null);
+  const [toppings, setTopping] = useState<Ingredients>(ingredients);
+
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example with TypeScript
-        </Typography>
-      </Box>
-    </Container>
+    <Grid container>
+      <Typography variant='h4' component='h1' align='center' gutterBottom>
+        IT'S PIZZA TIME
+      </Typography>
+      <Steps />
+    </Grid>
   );
 }
 
---
+++ src/components/Steps.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+    },
+    backButton: {
+      marginRight: theme.spacing(1),
+    },
+    instructions: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+    },
+  })
+);
+
+function getSteps() {
+  return [
+    'Choose a pizza size',
+    'Choose a crust type',
+    'Pick your toppings',
+    'Confirm your order'
+  ];
+}
+
+function getStepContent(stepIndex: number) {
+  switch (stepIndex) {
+    case 0:
+      return 'Choose a pizza size';
+    case 1:
+      return 'Choose a crust type';
+    case 2:
+      return 'Pick your toppings';
+    case 3:
+      return 'Confirm your order';
+    default:
+      return 'Unknown stepIndex';
+  }
+}
+
+function Steps() {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography className={classes.instructions}>
+              All steps completed
+            </Typography>
+            <Button onClick={handleReset}>Reset</Button>
+          </div>
+        ) : (
+          <div>
+            <Typography className={classes.instructions}>
+              {getStepContent(activeStep)}
+            </Typography>
+            <div>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.backButton}
+              >
+                Back
+              </Button>
+              <Button variant='contained' color='primary' onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Steps;
